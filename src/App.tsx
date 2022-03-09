6b8e90894c6f377dbba531d39a3bfa33153c74c2
Storm <storm_sia_llamas@yahoo.com>
2022-03-09 13:06:36 +0800

    marketplace redirect

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React, { Fragment } from "react";
 import { Provider } from "react-redux";
 import store from "./store";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
 // import Sidebar from "./components/layout/Sidebar/Sidebar";
 import MarketPlace from "./components/MarketPlace";
@@ -15,6 +20,7 @@ const App = () => {
       <Fragment>
         <Router>
           <Routes>
+            <Route path="/" element={<Navigate to="/marketplace/grid/id" />} />
             <Route
               path="/marketplace/:format/:page"
               element={<MarketPlace />}
